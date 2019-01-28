fe2185af20dee33869afe86c368f182530053ce7
DLorell <d.lorell@yahoo.com>
2019-01-28 10:25:28 -0800

    NN

--- Car/NNArchitecture.py
+++ Car/NNArchitecture.py
@@ -25,9 +25,9 @@ class MyGRU(nn.Module):
             hidden = self.initHidden()
             
             #print(hidden[0][0][0].item())
-            print(type(input))
-            print(input.dtype)
-            print(input.size())
+            print(type(input[0]))
+            print(input[0].dtype)
+            print(input[0].size())
         
             output, _ = self.network(input.cuda(), hidden.cuda())
             squeezed = self.linear(output[-1].cuda()).cuda()
