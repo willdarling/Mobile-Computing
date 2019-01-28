7848e445903e5142c7ab5161427dbca4b47f1756
DLorell <d.lorell@yahoo.com>
2019-01-28 10:43:30 -0800

    NN

--- Car/main.py
+++ Car/main.py
@@ -80,7 +80,7 @@ def train(model, optimizer, criterion, num_epochs, trainloader, devloader):
 
             y_hat = model(batch.cuda())
             
-            batch_targets = tensor.stack(batch_targets)
+            batch_targets = torch.stack(batch_targets)
             print(batch_targets)
 
             loss = criterion(y_hat.cuda(), batch_targets.cuda())
